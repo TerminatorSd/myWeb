4f03e4790eea4845b96c312b12e488204bef7521
sdMac <553828127@qq.com>
2019-04-24 19:50:24 +0800

    update 3000

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 
 var url = location.href.split('#')[0];     //获取当前地址，切记微信是要筛出#后面的地址
-$.post("http://39.108.163.91/getSignature",
+$.post("http://39.108.163.91:3000/getSignature",
     {url:url},
     function(data){
 //通过config注入接口权限
